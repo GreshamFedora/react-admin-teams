da6e680173b7488fd2b97c2d4ef41cbbca9338d9
Gresham Fedora <gresham.fedora@gmail.com>
2023-01-19 17:48:29 -0500

    added in card functionality and selector

--- src/App.js
+++ src/App.js
@@ -1,14 +1,126 @@
 import * as React from 'react';
 import Header from './components/Header'
-import Footer from './components/Footer'
 import Employees from './components/Employees'
-
+import Footer from './components/Footer'
+import { useState, useEffect } from 'react'
 
 function App() {
+  
+  const [selectedTeam, setTeam] = useState(JSON.parse(localStorage.getItem('selectedTeam')) || "Team-B")
+
+  const [employees, setEmployees] = useState(JSON.parse(localStorage.getItem('employeeList')) || [{
+  id: 1,
+  fullName: "Bob Jones",
+  designation: "JavaScript Developer",
+  gender: "male",
+  teamName: "Team-A"
+},
+{
+  id: 2,
+  fullName: "Jill Bailey",
+  designation: "Node Developer",
+  gender: "female",
+  teamName: "Team-A"
+},
+{
+  id: 3,
+  fullName: "Gail Shepherd",
+  designation: "Java Developer",
+  gender: "female",
+  teamName: "Team-A"
+},
+{
+  id: 4,
+  fullName: "Sam Reynolds",
+  designation: "React Developer",
+  gender: "male",
+  teamName: "Team-B"
+},
+{
+  id: 5,
+  fullName: "David Henry",
+  designation: "DotNet Developer",
+  gender: "male",
+  teamName: "Team-B"
+},
+{
+  id: 6,
+  fullName: "Sarah Blake",
+  designation: "SQL Server DBA",
+  gender: "female",
+  teamName: "Team-B"
+},
+{
+  id: 7,
+  fullName: "James Bennet",
+  designation: "Angular Developer",
+  gender: "male",
+  teamName: "Team-C"
+},
+{
+  id: 8,
+  fullName: "Jessica Faye",
+  designation: "API Developer",
+  gender: "female",
+  teamName: "Team-C"
+},
+{
+  id: 9,
+  fullName: "Lita Stone",
+  designation: "C++ Developer",
+  gender: "female",
+  teamName: "Team-C"
+},
+{
+  id: 10,
+  fullName: "Daniel Young",
+  designation: "Python Developer",
+  gender: "male",
+  teamName: "Team-D"
+},
+{
+  id: 11,
+  fullName: "Adrian Jacobs",
+  designation: "Vue Developer",
+  gender: "male",
+  teamName: "Team-D"
+},
+{
+  id: 12,
+  fullName: "Devin Monroe",
+  designation: "Graphic Designer",
+  gender: "male",
+  teamName: "Team-D"
+  }]);
+
+  useEffect(() => {
+
+    localStorage.setItem('employeeList',JSON.stringify(employees));
+
+  }, [employees]);
+
+  useEffect(() => {
+
+    localStorage.setItem('selectedTeam',JSON.stringify(selectedTeam));
+
+  }, [selectedTeam]);
+
+  function handleTeamSelectionChange(e) {
+      setTeam(e.target.value);
+  }
+
+  function handleEmployeeCardClick(e) {
+      const transformedEmployees = employees.map((employee) => employee.id === parseInt(e.currentTarget.id) 
+                                                          ? (employee.teamName === selectedTeam)?{...employee, teamName: ''}:{...employee, teamName: selectedTeam}
+                                                          :employee);
+  setEmployees(transformedEmployees);
+                                                      
+}
+
   return (
     <div>
-      <Header />
-      <Employees />      
+      <Header selectedTeam={selectedTeam} teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length} />
+      <Employees employees={employees} selectedTeam={selectedTeam} handleEmployeeCardClick={handleEmployeeCardClick} handleTeamSelectionChange={handleTeamSelectionChange} />      
       <Footer />
     </div>
   );
--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,105 +1,18 @@
-import { useState} from 'react'
 import femaleProfile from '../images/femaleProfile.jpg';
 import maleProfile from '../images/maleProfile.jpg';
  
-const Employees = () => {
+const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}) => {
 
-    const [selectedTeam, setTeam] = useState('TeamB')
-
-    const [employees, setEmployees] = useState([{
-    id: 1,
-    fullName: "Bob Jones",
-    designation: "JavaScript Developer",
-    gender: "male",
-    teamName: "TeamA"
-  },
-  {
-    id: 2,
-    fullName: "Jill Bailey",
-    designation: "Node Developer",
-    gender: "female",
-    teamName: "TeamA"
-  },
-  {
-    id: 3,
-    fullName: "Gail Shepherd",
-    designation: "Java Developer",
-    gender: "female",
-    teamName: "TeamA"
-  },
-  {
-    id: 4,
-    fullName: "Sam Reynolds",
-    designation: "React Developer",
-    gender: "male",
-    teamName: "TeamB"
-  },
-  {
-    id: 5,
-    fullName: "David Henry",
-    designation: "DotNet Developer",
-    gender: "male",
-    teamName: "TeamB"
-  },
-  {
-    id: 6,
-    fullName: "Sarah Blake",
-    designation: "SQL Server DBA",
-    gender: "female",
-    teamName: "TeamB"
-  },
-  {
-    id: 7,
-    fullName: "James Bennet",
-    designation: "Angular Developer",
-    gender: "male",
-    teamName: "TeamC"
-  },
-  {
-    id: 8,
-    fullName: "Jessica Faye",
-    designation: "API Developer",
-    gender: "female",
-    teamName: "TeamC"
-  },
-  {
-    id: 9,
-    fullName: "Lita Stone",
-    designation: "C++ Developer",
-    gender: "female",
-    teamName: "TeamC"
-  },
-  {
-    id: 10,
-    fullName: "Daniel Young",
-    designation: "Python Developer",
-    gender: "male",
-    teamName: "TeamD"
-  },
-  {
-    id: 11,
-    fullName: "Adrian Jacobs",
-    designation: "Vue Developer",
-    gender: "male",
-    teamName: "TeamD"
-  },
-  {
-    id: 12,
-    fullName: "Devin Monroe",
-    designation: "Graphic Designer",
-    gender: "male",
-    teamName: "TeamD"
-    }]);
 
     return (
       <main className='container'>
           <div className='row justify-content-center mt-3 mb-3'>
               <div className='col-8'>
-                  <select className='form-select form-select-lg'>
-                      <option value='TeamA'>Team A</option>
-                      <option value='TeamB'>Team B</option>
-                      <option value='TeamC'>Team C</option>
-                      <option value='TeamD'>Team D</option>
+                  <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
+                      <option value='Team-A'>Team-A</option>
+                      <option value='Team-B'>Team-B</option>
+                      <option value='Team-C'>Team-C</option>
+                      <option value='Team-D'>Team-D</option>
                   </select>
               </div>
           </div>
@@ -109,7 +22,7 @@ const Employees = () => {
                   <div className='card-collection'>
                       {
                       employees.map((employee) => (
-                          <div id={employee.id} classname="card" className='card m-3' style={{ cursor: "pointer" }}>
+                          <div key={employee.id} id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
 
                               {(employee.gender === 'male') ? <img src={maleProfile} className="card-img-top" />
                                                             : <img src={femaleProfile} className="card-img-top" />}

abfb9ab1046646097430f46008e8c6211da6840e
Gresham Fedora <gresham.fedora@gmail.com>
2023-01-19 15:06:29 -0500

    added in cards and styling for employees

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,7 +1,12 @@
 import { useState} from 'react'
+import femaleProfile from '../images/femaleProfile.jpg';
+import maleProfile from '../images/maleProfile.jpg';
  
 const Employees = () => {
-const [employees, setEmployees] = useState([{
+
+    const [selectedTeam, setTeam] = useState('TeamB')
+
+    const [employees, setEmployees] = useState([{
     id: 1,
     fullName: "Bob Jones",
     designation: "JavaScript Developer",
@@ -84,16 +89,44 @@ const [employees, setEmployees] = useState([{
     designation: "Graphic Designer",
     gender: "male",
     teamName: "TeamD"
-  }])
-  return (
-    <main>
-        {
-        employees.map((employee) => (
-            <p>{employee.fullName}</p>
-        ))
-        }
-    </main>
-  )
+    }]);
+
+    return (
+      <main className='container'>
+          <div className='row justify-content-center mt-3 mb-3'>
+              <div className='col-8'>
+                  <select className='form-select form-select-lg'>
+                      <option value='TeamA'>Team A</option>
+                      <option value='TeamB'>Team B</option>
+                      <option value='TeamC'>Team C</option>
+                      <option value='TeamD'>Team D</option>
+                  </select>
+              </div>
+          </div>
+
+          <div className='row justify-content-center mt-3 mb-3'>
+              <div className='col-8'>
+                  <div className='card-collection'>
+                      {
+                      employees.map((employee) => (
+                          <div id={employee.id} classname="card" className='card m-3' style={{ cursor: "pointer" }}>
+
+                              {(employee.gender === 'male') ? <img src={maleProfile} className="card-img-top" />
+                                                            : <img src={femaleProfile} className="card-img-top" />}
+
+                              <div className='card-body'>
+                                  <h5 className='card-title'>Full Name: {employee.fullName}</h5>
+                                  <p className='card-text'><b>Designation:</b> {employee.designation}</p>
+                              </div>
+                          </div>
+                      ))
+                      }
+                  </div>
+              </div>
+          </div>
+                    
+      </main>
+    )
 }
 
 export default Employees

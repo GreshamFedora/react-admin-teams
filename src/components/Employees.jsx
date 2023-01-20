571b7ffc53298afc65d9388dfd5e97868efd4905
Gresham Fedora <gresham.fedora@gmail.com>
2023-01-20 14:38:12 -0500

    still abstracting

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,5 +1,6 @@
-import femaleProfile from '../images/femaleProfile.jpg';
-import maleProfile from '../images/maleProfile.jpg';
+import Teams from './Teams';
+import TeamMembers from './TeamMembers';
+
  
 const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}) => {
 
@@ -8,32 +9,16 @@ const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeam
       <main className='container'>
           <div className='row justify-content-center mt-3 mb-3'>
               <div className='col-8'>
-                  <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
-                      <option value='Team-A'>Team-A</option>
-                      <option value='Team-B'>Team-B</option>
-                      <option value='Team-C'>Team-C</option>
-                      <option value='Team-D'>Team-D</option>
-                  </select>
+                <Teams selectedTeam={selectedTeam} handleTeamSelectionChange={handleTeamSelectionChange}/>
               </div>
           </div>
 
           <div className='row justify-content-center mt-3 mb-3'>
               <div className='col-8'>
                   <div className='card-collection'>
-                      {
-                      employees.map((employee) => (
-                          <div key={employee.id} id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
-
-                              {(employee.gender === 'male') ? <img src={maleProfile} className="card-img-top" />
-                                                            : <img src={femaleProfile} className="card-img-top" />}
-
-                              <div className='card-body'>
-                                  <h5 className='card-title'>Full Name: {employee.fullName}</h5>
-                                  <p className='card-text'><b>Designation:</b> {employee.designation}</p>
-                              </div>
-                          </div>
-                      ))
-                      }
+                        <TeamMembers employees={employees} 
+                                     handleEmployeeCardClick={handleEmployeeCardClick} 
+                                     selectedTeam={selectedTeam}/>
                   </div>
               </div>
           </div>
